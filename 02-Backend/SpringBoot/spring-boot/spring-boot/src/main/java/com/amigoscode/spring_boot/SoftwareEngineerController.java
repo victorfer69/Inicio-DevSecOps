36d98d587ea79bcb5bd17f7b9750f026972c7900
victorfer69 <victor.fernandez0609@gmail.com>
2025-11-10 11:05:13 +0100

    REST API devuelve un JSON

---
+++ 02-Backend/SpringBoot/spring-boot/spring-boot/src/main/java/com/amigoscode/spring_boot/SoftwareEngineerController.java
@@ -0,0 +1,21 @@
+package com.amigoscode.spring_boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers(){
+        return List.of(
+                new SoftwareEngineer(1, "James", "js, node, react"),
+                new SoftwareEngineer(2, "Camila", "java, spring")
+        );
+    }
+
+}
